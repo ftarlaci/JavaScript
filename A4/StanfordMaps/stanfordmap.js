a923af2c12069c7732185d33b033574dcd21c452
coderphd <18291571+ftarlaci@users.noreply.github.com>
2018-07-31 20:41:21 -0500

    Stanford map js

---
+++ A4/StanfordMaps/stanfordmap.js
@@ -0,0 +1,21 @@
+"use strict";
+
+
+
+
+
+var mapFrame = document.getElementById("mapFrame");
+
+
+
+
+
+document.addEventListener("mousemove",handleMouseMove);
+document.addEventListener("mousedown",handleMouseDown);
+document.addEventListener("mouseup",handleMouseUp);
+document.addEventListener("dblclick",handleDblClick);
+window.addEventListener("resize",resizeFrame);
+
+
+
+
